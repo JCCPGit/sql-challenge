64b891fb2240094373c2f2abead2278d8cf64aed
JCCPGit <cortezperezjc@gmail.com>
2021-05-20 14:29:58 -0700

    Data analysis advancement

---
+++ EmployeeSQL/data/challenge_queries.sql
@@ -0,0 +1,62 @@
+-- Content query
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+-- 1. List the following details of each employee: employee number, last name, first name, sex and salary
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no = salaries.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01'
+AND hire_date <= '1986-12-31';
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's
+-- employee number, last name, first name
+SELECT mng.dept_no, departments.dept_name, mng.emp_no, employees.last_name, employees.first_name
+FROM dept_manager AS mng
+LEFT JOIN departments ON
+departments.dept_no = mng.dept_no
+LEFT JOIN employees ON
+mng.emp_no = employees.emp_no;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+LEFT JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments ON
+dept_emp.dept_no = departments.dept_no;
+
+-- 5. List first name, last name, and sex for employess whose first name is "Hercules" and last names begin with "B"
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON
+dept_emp.emp_no = employees.emp_no
+JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON
+dept_emp.emp_no = employees.emp_no
+JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Sales'
+OR dept_name = 'Development';
+
